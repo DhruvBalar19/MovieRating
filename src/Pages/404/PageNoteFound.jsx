4052b4fc543ea442b6783182f795b81c23a7085e
DhruvBalar19 <dhruvbalar22@gmail.com>
2023-06-29 14:09:09 +0530

    Movie and tv show Rating app

--- src/Pages/404/PageNoteFound.jsx
+++ src/Pages/404/PageNoteFound.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./Style.scss"
+import ContentWrapper from '../../Componates/ContantWrapper/ContantWrapper'
 
 function PageNoteFound() {
   return (
     <div>
-      
+          return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
     </div>
   )
 }
